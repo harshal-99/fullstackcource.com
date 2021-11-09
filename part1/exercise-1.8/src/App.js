578429ce4e224f2e6baa89c9004f7bb6fd89d5b7
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-09 22:58:03 +0530

    Done exercise 1.8

--- part1/exercise-1.8/src/App.js
+++ part1/exercise-1.8/src/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Button                         from "./components/Button";
 
-const Statistics = (props) => {
+const App = () => {
+	// save clicks of each button to its own state
+	const [ good, setGood ] = useState(0)
+	const [ bad, setBad ] = useState(0)
+	const [ neutral, setNeutral ] = useState(0)
+
 	const [ all, setAll ] = useState(0)
 	const [ average, setAverage ] = useState(0)
 	const [ positive, setPositive ] = useState(0)
-
-	const {good, bad, neutral} = props
 
 	useEffect(() => {
 		setAll(good + bad + neutral)
@@ -14,21 +17,6 @@ const Statistics = (props) => {
 		let res = all ? (good * 100) / all : 0
 		setPositive(res)
 	}, [ good, bad, neutral, all ])
-	return (
-		<>
-			<div>{"all " + all}</div>
-			<div>{"average " + average}</div>
-			<div>{"positive " + positive + "%"}</div>
-		</>
-	)
-}
-
-const App = () => {
-	// save clicks of each button to its own state
-	const [ good, setGood ] = useState(0)
-	const [ bad, setBad ] = useState(0)
-	const [ neutral, setNeutral ] = useState(0)
-
 
 	return (
 		<div>
@@ -47,8 +35,9 @@ const App = () => {
 			<h2>statistics</h2>
 			<div>{"good " + good}</div>
 			<div>{"neutral " + neutral}</div>
-			<div>{"bad " + bad}</div>
-			<Statistics good={good} bad={bad} neutral={neutral}/>
+			<div>{"all " + all}</div>
+			<div>{"average " + average}</div>
+			<div>{"positive " + positive + "%"}</div>
 		</div>
 	)
 }
