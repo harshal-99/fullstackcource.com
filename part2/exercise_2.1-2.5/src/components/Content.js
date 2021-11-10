64c6013b76e09c2080a94d600eb6f72baea4acb3
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-10 23:08:20 +0530

    Done exercise 2.3

--- part2/exercise_2.1-2.5/src/components/Content.js
+++ part2/exercise_2.1-2.5/src/components/Content.js
@@ -5,13 +5,13 @@ import classes from "./Content.module.css";
 
 
 const Content = ({parts}) => {
-	let exercise = 0
-	for (const part of parts) {
-		exercise += part.exercises
-	}
+	let exercise = parts.reduce(
+		(prev, curr) => prev + curr.exercises, 0
+	)
+
 	return (
 		<div>
-			{parts.map(part => <Part part={part}/>)}
+			{parts.map(part => <Part key={part.name} part={part}/>)}
 			<div className={classes.bold}>total of {exercise} exercises
 			</div>
 		</div>
