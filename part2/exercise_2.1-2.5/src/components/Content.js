43b438b69ad3f2d5d2ca50e73cd01511f9842072
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-10 22:57:51 +0530

    Done exercise 2.2

--- part2/exercise_2.1-2.5/src/components/Content.js
+++ part2/exercise_2.1-2.5/src/components/Content.js
@@ -1,10 +1,19 @@
 import React from "react";
-import Part  from "./Part";
+
+import Part    from "./Part";
+import classes from "./Content.module.css";
+
 
 const Content = ({parts}) => {
+	let exercise = 0
+	for (const part of parts) {
+		exercise += part.exercises
+	}
 	return (
 		<div>
 			{parts.map(part => <Part part={part}/>)}
+			<div className={classes.bold}>total of {exercise} exercises
+			</div>
 		</div>
 	)
 }
