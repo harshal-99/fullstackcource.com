0afe419903eccdc7dd1870d0916aba8c2ba1f1ad
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-15 19:53:19 +0530

    Done exercise 2.11

--- part2/exercise_2.6-2.10/src/App.js
+++ part2/exercise_2.6-2.10/src/App.js
@@ -1,19 +1,21 @@
-import { useState } from "react";
-import Names        from "./components/Names";
-import PersonForm   from "./components/PersonForm";
+import { useEffect, useState } from "react";
+import axios                   from "axios";
+
+import Names      from "./components/Names";
+import PersonForm from "./components/PersonForm";
 
 const App = () => {
-	const [ person, setPerson ] = useState([
-		{name: 'Arto Hellas', number: '040-123456', id: 1},
-		{name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-		{name: 'Dan Abramov', number: '12-43-234345', id: 3},
-		{name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-	])
-
+	const [ person, setPerson ] = useState([])
 	const [ newName, setNewName ] = useState("")
 	const [ newNumber, setNewNumber ] = useState("")
 	const [ search, setSearch ] = useState("")
 	const [ searchResult, setSearchResult ] = useState([])
+
+	useEffect(() => {
+		axios
+			.get("http://localhost:3001/persons")
+			.then(res => setPerson(res.data))
+	}, [])
 
 	const addName = (event) => {
 		event.preventDefault()
