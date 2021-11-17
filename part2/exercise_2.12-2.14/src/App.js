56caeebe0d5a1733c6910c89f1063d70d10d38e9
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-17 19:31:47 +0530

    Done exercise 2.12

--- part2/exercise_2.12-2.14/src/App.js
+++ part2/exercise_2.12-2.14/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
-import axios from "axios";
-
+import axios   from "axios";
+import classes from "./App.module.css"
 
 const App = () => {
 	const [ searchInput, setSearchInput ] = useState("")
@@ -25,9 +25,20 @@ const App = () => {
 			.catch(err => console.log(err))
 	}, [])
 
+	const showDetailedView = (name, capital, region, flag) => {
+		return (
+			<div>
+				<h1>{name}</h1>
+				<div>capital {capital}</div>
+				<div>region {region} < /div>
+				<div
+					className={classes.big}>{flag}</div>
+			</div>
+		)
+	}
 
 	return (
-		<form>
+		<form onSubmit={event => event.preventDefault()}>
 			<div>
 				<input type="text" value={searchInput}
 				       onChange={handleInputChange}/>
@@ -37,12 +48,15 @@ const App = () => {
 					{searchResults.length <= 10 ?
 						searchResults.length === 1 ?
 							<div>
-								<h1>{searchResults[0].name["common"]}</h1>
-								<div>capital {searchResults[0]["capital"][0]}</div>
-								<div>region {searchResults[0]["region"]} < /div>
+								{showDetailedView(searchResults[0].name["common"],
+									searchResults[0]["capital"][0],
+									searchResults[0]["region"],
+									searchResults[0]["flag"])}
 							</div> :
-							searchResults.map(country => <div
-								key={country["fifa"]}>{country.name["common"]}</div>)
+							searchResults.map((country, index) => <div
+								key={country["fifa"]}>
+								{country.name["common"]}
+							</div>)
 						: <div key={Math.random() * 10}>Too many matches, specify
 							your query</div>
 					}
