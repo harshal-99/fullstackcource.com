0611fb3958548e0deb2149c81d50917d2a000ca3
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2021-11-18 19:40:34 +0530

    Done exercise 2.12

--- part2/exercise_2.12-2.14/src/App.js
+++ part2/exercise_2.12-2.14/src/App.js
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
 
-import axios   from "axios";
+import axios  from "axios";
+import dotenv from "dotenv"
+
 import classes from "./App.module.css"
+
+dotenv.config()
 
 const App = () => {
 	const [ searchInput, setSearchInput ] = useState("")
 	const [ searchResults, setSearchResults ] = useState([])
 	const [ countries, setCountries ] = useState([])
+
+	const baseURL = `http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=`
 
 	const handleInputChange = (event) => {
 		event.preventDefault()
@@ -25,6 +31,19 @@ const App = () => {
 			.catch(err => console.log(err))
 	}, [])
 
+	const getWeatherData = () => {
+		countries.forEach(country => {
+			let result = axios
+				.get(baseURL + country["capital"])
+				.then(res => res.data)
+			console.log(result['current'])
+			country['temperature'] = result['current']['temperature']
+			country['weather_icons'] = result['current']['weather_icons'][0]
+			country['wind_speed'] = result['current']['wind_speed']
+			country['wind_dir'] = result['current']['wind_dir']
+		})
+	}
+	getWeatherData()
 	const showDetailedView = (name, capital, region, flag) => {
 		return (
 			<div>
@@ -52,6 +71,8 @@ const App = () => {
 									searchResults[0]["capital"][0],
 									searchResults[0]["region"],
 									searchResults[0]["flag"])}
+								<h2>Weather in {searchResults[0]["capital"][0]}</h2>
+								<div>temperature: {searchResults[0]["temperature"]} Census</div>
 							</div> :
 							searchResults.map((country, index) => <div
 								key={country["fifa"]}>
