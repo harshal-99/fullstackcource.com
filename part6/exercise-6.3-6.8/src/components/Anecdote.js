ef3f230b7f2b41acf8007b9f8ee3830c06608191
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-01-15 12:16:59 +0530

    Done exercise 6.5.
    Anecdotes are sorted by the number of votes.

--- part6/exercise-6.3-6.8/src/components/Anecdote.js
+++ part6/exercise-6.3-6.8/src/components/Anecdote.js
@@ -15,7 +15,7 @@ const Anecdote = ({anecdote, handleClick}) => {
 
 const Anecdotes = () => {
 	const dispatch = useDispatch()
-	const anecdotes = useSelector(state => state)
+	const anecdotes = useSelector(state => [...state].sort((a, b) => b.votes - a.votes))
 
 	return (
 		<ul>
