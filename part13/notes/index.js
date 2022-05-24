a0021da71b2eb620be5c400317f70a6a4aad800f
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-05-24 14:57:05 +0530

    Add userRouter and loginRouter to index.js

--- part13/notes/index.js
+++ part13/notes/index.js
@@ -3,6 +3,8 @@ import morgan from "morgan"
 import noteRouter from "./controllers/notes.js";
 import {connectToDatabase} from "./util/db.js";
 import {PORT} from "./util/config.js";
+import usersRouter from "./controllers/users.js";
+import loginRouter from "./controllers/login.js";
 
 const app = express()
 
@@ -12,7 +14,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/notes', noteRouter)
-
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
 	await connectToDatabase()
