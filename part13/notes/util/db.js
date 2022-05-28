ddcdf10e58cdb693630c9c0e89c82d12901d4ee9
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-05-28 10:08:12 +0530

    Implement migration using umzug

--- part13/notes/util/db.js
+++ part13/notes/util/db.js
@@ -1,7 +1,23 @@
 import {Sequelize} from "sequelize";
 import {DATABASE_URL} from "./config.js";
+import {SequelizeStorage, Umzug} from "umzug";
 
 export const sequelize = new Sequelize(DATABASE_URL)
+
+export const runMigrations = async () => {
+	const migrator = new Umzug({
+		migrations: {
+			glob: 'migrations/*.js'
+		},
+		storage: new SequelizeStorage({sequelize, tableName: 'migrations'}),
+		context: sequelize.getQueryInterface(),
+		logger: console
+	})
+	const migrations = await migrator.up()
+	console.log('Migrations up to date', {
+		files: migrations.map(mig => mig.name)
+	})
+}
 
 export const connectToDatabase = async () => {
 	try {
