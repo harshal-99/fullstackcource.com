9dad51656f99d7109d8934b346515677be621020
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-12 19:10:22 +0530

    Implement post route for patient

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,26 @@
-import {Router}                     from "express";
-import {getNonSensitivePatientInfo} from "../services/patientsService";
+import {Router}                                 from "express";
+import {addPatient, getNonSensitivePatientInfo} from "../services/patientsService";
+import {toNewPatientEntry}                      from "../utils";
 
 const patientsRouter = Router()
 
 patientsRouter.get('/', (_request, response) => {
   response.json(getNonSensitivePatientInfo())
+})
+
+patientsRouter.post('/', (request, response) => {
+  try {
+    const newPatient = toNewPatientEntry(request.body)
+
+    const addedEntry = addPatient(newPatient)
+    response.json(addedEntry)
+  } catch (error) {
+    let errorMessage = 'Something went wrong'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message
+    }
+    response.status(400).send(errorMessage)
+  }
 })
 
 export default patientsRouter
