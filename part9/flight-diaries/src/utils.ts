6c8c4e1484ce2e89355a7e042a1094da7b758768
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-12 18:25:30 +0530

    Implement input validation

---
+++ part9/flight-diaries/src/utils.ts
@@ -0,0 +1,57 @@
+import {NewDiaryEntry, Visibility, Weather} from "./types";
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String
+}
+
+const parseComment = (comment: unknown): string => {
+  if (!comment || !isString(comment)) {
+    throw  new Error('Incorrect or missing comment')
+  }
+  return comment
+}
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date))
+}
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date: ' + date)
+  }
+  return date
+}
+
+const isWeather = (param: any): param is Weather => {
+  return Object.values(Weather).includes(param)
+}
+
+const parseWeather = (weather: unknown): Weather => {
+  if (!weather || !isWeather(weather)) {
+    throw new Error('Incorrect or missing weather: ' + weather)
+  }
+  return weather
+}
+
+const isVisibility = (param: any): param is Visibility => {
+  return Object.values(Visibility).includes(param)
+}
+
+const parseVisibility = (visibility: unknown): Visibility => {
+  if (!visibility || !isVisibility(visibility)) {
+    throw new Error('Incorrect or missing visibility: ' + visibility)
+  }
+  return visibility
+}
+
+type Fields = { comment: unknown, date: unknown, weather: unknown, visibility: unknown }
+
+export const toNewDiaryEntry = ({comment, date, weather, visibility}: Fields): NewDiaryEntry => {
+  return {
+    comment: parseComment(comment),
+    date: parseDate(date),
+    weather: parseWeather(weather),
+    visibility: parseVisibility(visibility)
+
+  }
+}
