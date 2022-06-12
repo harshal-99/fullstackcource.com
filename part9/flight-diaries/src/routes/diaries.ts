42ae2caf8f78b3e68cd3bd53d5befe6c81b7bb1c
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-12 18:24:34 +0530

    Implement post for diaries

--- part9/flight-diaries/src/routes/diaries.ts
+++ part9/flight-diaries/src/routes/diaries.ts
@@ -1,5 +1,6 @@
-import {Router}                 from "express";
-import {getNonSensitiveEntries} from "../services/diaryService";
+import {Router}                                     from "express";
+import {addDiary, findById, getNonSensitiveEntries} from "../services/diaryService";
+import {toNewDiaryEntry}                            from "../utils";
 
 const router = Router()
 
@@ -7,8 +8,29 @@ router.get('/', (_request, response) => {
   response.send(getNonSensitiveEntries())
 })
 
-router.post('/', (_request, response) => {
-  response.send('Saving a diary!')
+router.post('/', (request, response) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(request.body)
+
+    const addedEntry = addDiary(newDiaryEntry)
+    response.json(addedEntry)
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong'
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message
+    }
+    response.status(400).send(errorMessage)
+  }
+})
+
+router.get('/:id', (request, response) => {
+  const diary = findById(Number(request.params.id))
+
+  if (diary) {
+    response.send(diary)
+  } else {
+    response.sendStatus(404)
+  }
 })
 
 export default router
