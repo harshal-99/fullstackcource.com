7e2d1d709697fe88b6bd23900213094f45e97e49
Harshal Pawshekar <harshalpawshekar999@gmail.com>
2022-06-16 19:39:28 +0530

    Extend base entity to accommodate HospitalEntry, OccupationalHealthcareEntry and HealthCheckEntry

--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -23,5 +23,48 @@ export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>
 
 export type NewPatient = Omit<Patient, 'id'>
 
-export interface Entry {
+export interface BaseEntry {
+  id: string
+  description: string
+  date: string
+  specialist: string
+  diagnosisCodes?: Array<Diagnose['code']>
 }
+
+export enum HealthCheckRating {
+  "Healthy",
+  "LowRisk",
+  "HighRisk",
+  "CriticalRisk"
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck"
+  healthCheckRating: HealthCheckRating
+}
+
+interface Discharge {
+  date: string
+  criteria: string
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital"
+  discharge: Discharge
+}
+
+interface SickLeave {
+  startDate: string
+  endDate: string
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare"
+  employerName: string
+  sickLeave?: SickLeave
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry
